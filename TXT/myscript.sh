b1d5de834afa54ab3f09f1f41be961e4357b6c94
Donia Sakji <124528589+Nodnod77@users.noreply.github.com>
2024-10-01 00:03:39 +0700

    add encrypted file

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -16,7 +16,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="7FCBFC7FBE607C1A"
+REC2="C8214F68261CF03B"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
